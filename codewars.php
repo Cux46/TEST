96132c45e96fe266b7cca9e7bf89b0e15020608d
Dino Nicotera <dino.nicotera@gmail.com>
2025-01-14 11:11:21 +0100

    aggiunto script.php

--- codewars.php
+++ codewars.php
@@ -1,9 +1,3 @@
 <?php
-function past($h, $m, $s) {
-  if ($h >= 0 && $h <= 23 && $m >= 0 && $m <= 60 && $s >= 0 && $s <= 60) {
-    $ms = ($h * 3600000) + ($m * 60000) + ($s * 1000);
-  }
-  echo $ms;
-}
-
-past(0,1,1);
+const NOME = `pippo`;
+echo  NOME;
